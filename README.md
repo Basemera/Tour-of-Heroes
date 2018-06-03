88ad15e7704f12edb7fc2de331011ab0ab02e687
Basemera <phiona.basemera@andela.com>
2018-06-04 01:41:10 +0300

    initializing the project

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # TourOfHeros
+This is an Angular project to help me learn Angular.
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.7.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+export class Hero {
+  name: string;
+  id: number;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'The Tour of heros';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+  otherHeroes = ['Sam', 'Joshua', 'David'];
 }
